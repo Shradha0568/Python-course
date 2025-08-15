f251b3207297a68cd20e7288bb580c39c4e68645
Shradha Arsewar <shradha.arsewar@neutrinotechlabs.com>
2025-08-15 23:59:12 +0530

    Practicing API handling

---
+++ Python Project/flask_api.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a+b
+
+print(add(2,3))\
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
